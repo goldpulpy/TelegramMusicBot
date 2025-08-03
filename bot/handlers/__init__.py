0e3863f97cc37c983d3b5549c385d8e4a757869f
goldpulpy <sawsani1928@gmail.com>
2025-08-04 01:15:23 +0300

    feat: add IsPrivateChatFilter to handle private chat scenarios in the bot

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -2,6 +2,8 @@
 
 from aiogram import Dispatcher, Router
 
+from bot.filters import IsPrivateChatFilter
+
 from . import faq, get_track, language, menu, pages, search, subscribe
 
 
@@ -9,13 +11,17 @@ def setup(dp: Dispatcher) -> None:
     """Set up handlers for the bot."""
     router = Router()
 
+    # Add filters
+    router.message.filter(IsPrivateChatFilter())
+    router.callback_query.filter(IsPrivateChatFilter())
+
     # Register routers
     language.register(router)
     menu.register(router)
     faq.register(router)
-    subscribe.register(router)
     search.register(router)
     pages.register(router)
+    subscribe.register(router)
     get_track.register(router)
 
     dp.include_router(router)
